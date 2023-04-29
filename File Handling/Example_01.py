69674cf97ec34ce34d10752b6f8b40b560eae86d
ayan878 <ayanraza197@gmail.com>
2023-04-29 15:23:22 +0530

    fileHandling

--- File Handling/Example_01.py
+++ File Handling/Example_01.py
@@ -11,18 +11,36 @@ print("Is File Closed : ",file.closed)
 #writing data to the text files
 
 file= open("Example_01.txt",'w')
-file.write("It is Example_01.text file")
+file.write("It is Example_01.text file.")
 print("Data written to the file succesfully")
 file.close()
 
-# Open a file in write mode
+# Open a file in read mode
 file = open("Example_01.txt", 'r')
-data= file.read()
+data= file.read() #read total data from the file
 print(data)
 
+#open a file in write mode
 file = open("Example_01.txt",'w')
 list=["Ayan","Belal","Ehsan","Shazeb"]
 file.writelines(list)
 print("List of line written to the file succesfully")
 file.close()
 
+#To read only first 10 characters:
+file=open("Example_01.txt",'r')
+data=file.read(10)
+print(data)
+file.close()
+
+#To read data only one line
+file=open("Example_01.txt",'r')
+data=file.readline()
+print(data)
+file.close()
+
+#To read data line by line
+file=open("Example_01.txt",'r')
+data=file.readlines()
+print(data)
+file.close()
--- File Handling/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-file = open('File handling/Example_01.txt','w')
-# print("It is Example_01 file",file.Example_01.txt)
