aed1d71ad1e11f7150b7e0dacfcb706207daeb49
ayan878 <ayanraza197@gmail.com>
2023-04-29 14:20:11 +0530

    fileHandling

--- File Handling/Example_01.py
+++ File Handling/Example_01.py
@@ -1,9 +1,17 @@
 
 file=open("File Handling/Example_01.txt",'w')
-print("File Name: ",file.name)
-print("File Mode: ",file.mode)
-print("Is File Readable: ",file.readable())
-print("Is File Writable: ",file.writable())
-print("Is File Closed : ",file.closed)
+print("File Name: ",file.name) # Name of the opened file
+print("File Mode: ",file.mode) #mode of the opened file
+print("Is File Readable: ",file.readable()) #return boolean whether file readdable or not 
+print("Is File Writable: ",file.writable()) #return boolean wheather file writable  or or
+print("Is File Closed : ",file.closed) #return boolean file open or not
 file.close()
 print("Is File Closed : ",file.closed)
+
+#writing data to the text files
+
+file= open("Example_01.txt",'w')
+file.write("It is Example_01.text file")
+print("Data written to the file succesfully")
+file.close()
+
